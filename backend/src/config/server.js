0f1627317d7f816cc0e924539c98ac7f84dfae17
Filipe de Sousa Botelho <filipegame07@gmail.com>
2018-03-16 04:52:30 -0300

    Configurando o server

---
+++ backend/src/config/server.js
@@ -0,0 +1,13 @@
+const port = 3003;
+
+const bodyPaser = require('body-parser');
+const express = require('express');
+
+const server = express();
+
+server.use(bodyPaser.urlencoded({ extended: true }));
+server.use(bodyPaser.json());
+
+server.listen(port, () => {
+  console.log(`BACKEND is running on port ${port}.`);
+});
