ea3939ed86514dc7fbf9660e27406458787fecdc
Filipe de Sousa Botelho <filipegame07@gmail.com>
2018-03-27 22:42:03 -0300

    Habilitando o CORS

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -2,11 +2,13 @@ const port = 3003;
 
 const bodyPaser = require('body-parser');
 const express = require('express');
+const allowCors = require('./cors');
 
 const server = express();
 
 server.use(bodyPaser.urlencoded({ extended: true }));
 server.use(bodyPaser.json());
+server.use(allowCors);
 
 server.listen(port, () => {
   console.log(`BACKEND is running on port ${port}.`);
